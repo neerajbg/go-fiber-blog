b690ff172a433d1a8adeb5d28f836da399e7a505
neerajbg <neerajbg@gmail.com>
2023-08-31 19:05:45 +0530

    setup react app

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <Col xs="12 py-2">
+          <h1 className="text-center ">
+            React Application with Go fiber Backend
+          </h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
